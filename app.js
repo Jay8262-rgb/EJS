7c18ead09aac4d456fd8fc9b073e4ce81ae65dec
Jay8262-rgb <keshri425@gmail.com>
2025-10-23 22:20:24 +0530

    updateFinal

--- app.js
+++ app.js
@@ -1,38 +1,62 @@
+const express = require('express');
 
-const express = require("express");
-const bodyParser = require("body-parser");
-
-var app = express();
+const app = express();
 
 app.set("view engine", "ejs");
-app.use(express.urlencoded({extended:true }));
+
+app.use(express.urlencoded({ extended: true }));
 app.use(express.static('public'));
 
-var items = [];
-var example = "working";
+let items = [];
 
-
-app.get("/",function(req,res){
-    res.render("list",{ejs : items});
+app.get("/", function(req, res) {
+    res.render("list", { ejs: items, showUpdate: false});
 });
 
-app.post("/",function(req,res){
-    var item = req.body.ejs1;
-    items.push(item);
-    res.redirect('/');
+app.post("/", function(req, res) {
+    const item = req.body.ejs1 ? req.body.ejs1.trim() : "" ;
+    const priority = req.body.priority;
+    if (item.length > 0) { 
+        items.push({item, priority});
+    } else {
+        console.log("Enter a valid input value");
+        // return res.status(400).send("<h1>Plz insert valid data</h1>");
+    }
+    res.redirect("/");  
 });
 
-// app.post("/delete", (req, res) => {
-//     const id = parseInt(req.body.id);
-//     tasks = tasks.filter(t => t.id !== id);
-//     error = "";
-//     res.redirect("/");
-// });
-
-
-
-app.listen(4000,function(){
-    console.log("server is started");
+app.get("/edit/:id", function(req, res) {
+  const EdId = req.params.id;
+  const editValue = items[EdId];
+  res.render("list", {ejs: items, editIndex: EdId, editValue: editValue, showUpdate: true });
 });
 
- 
+app.post("/update/:id", function(req, res) {
+  const id = req.params.id;
+  const newValue = req.body.newValue.trim();
+  const priority = req.body.priority;
+
+  if (newValue.length > 0) {
+    
+    items[id] = {item : newValue , priority};
+  }else{
+    console.log("enter the input value");
+    // return res.status(400).send("<h1>Plz enter a value");
+  }
+  res.redirect("/");
+});
+
+
+
+app.post("/delete/:id" , function(req, res){
+  const StoredID = req.params.id;
+  const delData = items[StoredID];
+  console.log(delData);
+  items.splice(delData, 1);
+  res.redirect("/");  
+});
+
+
+app.listen(4004, function() {
+    console.log('Server is started on http://localhost:4004');
+});
